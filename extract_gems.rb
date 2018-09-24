697fb72e724638b5ed0e1011b817ce52c4f25e38
Jaci Brunning <jaci.brunning@gmail.com>
2018-09-24 17:52:38 +0800

    Remove gemfile= from config

--- extract_gems.rb
+++ extract_gems.rb
@@ -1,7 +1,11 @@
-if ENV['WEBCORE_LOADPATH']
+lp = '/etc/www/webcore/loadpath'
+if lp
   require 'fileutils'
-  FileUtils.mkdir_p ENV['WEBCORE_LOADPATH']
+  FileUtils.mkdir_p lp
+  puts "Loading from #{lp}"
   `bundle show --paths`.split("\n").map { |x| File.join(x, "lib") }.each do |d|
-    FileUtils.copy_entry(d, ENV['WEBCORE_LOADPATH'], false, true)
+    FileUtils.copy_entry(d, lp, false, true) if File.exist? d
   end
+else
+  puts "No loadpath!"
 end
--- fantasy_first/fantasy_first.webcore.rb
+++ fantasy_first/fantasy_first.webcore.rb
@@ -2,5 +2,4 @@ configure! do |m|
   m.id = :fantasy_first
   m.host = /fantasy\.first\.*/
   m.module = 'fantasy_first.rb'
-  m.gemfile = File.join(File.dirname(__FILE__), '../Gemfile')
 end
